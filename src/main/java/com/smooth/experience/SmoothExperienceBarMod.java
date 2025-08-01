23b5a23d464f1f4d8b164f26fa35dd508efe7e9e
soyouber <soyouber@outlook.com>
2025-08-01 13:03:03 +0800

    Fix

--- src/main/java/com/smooth/experience/SmoothExperienceBarMod.java
+++ src/main/java/com/smooth/experience/SmoothExperienceBarMod.java
@@ -3,7 +3,7 @@ package com.smooth.experience;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 
-@Mod("smooth-experience-bar")
+@Mod("smooth_experience_bar")
 public class SmoothExperienceBarMod {
     public SmoothExperienceBarMod() {
         MinecraftForge.EVENT_BUS.register(new SmoothExperiencebar());
