2e1dabcc6331d429b3ddd48b65aa63b65e14da54
soyouber <soyouber@outlook.com>
2025-08-14 11:50:32 +0800

    Update Minecraft 1.21.4

--- src/main/java/com/smooth/experience/mixin/InGameHudMixin.java
+++ src/main/java/com/smooth/experience/mixin/InGameHudMixin.java
@@ -1,6 +1,7 @@
 package com.smooth.experience.mixin;
 
 import com.smooth.experience.SmoothExperiencebar;
+import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.hud.InGameHud;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -10,23 +11,11 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(InGameHud.class)
 public abstract class InGameHudMixin {
     @Inject(
-            method = "renderExperienceBar",
+            method = "renderExperienceBar(Lnet/minecraft/client/gui/DrawContext;I)V",
             at = @At("HEAD"),
             cancellable = true
     )
-    private void disableVanillaExperienceBar(CallbackInfo ci) {
-        if (SmoothExperiencebar.autohudDetected) return;
-        if (SmoothExperiencebar.shouldDisableVanillaExperienceBar()) {
-            ci.cancel();
-        }
-    }
-
-    @Inject(
-            method = "renderExperienceLevel",
-            at = @At("HEAD"),
-            cancellable = true
-    )
-    private void disableVanillaExperienceLevel(CallbackInfo ci) {
+    private void disableVanillaExperienceBar(DrawContext context, int x, CallbackInfo ci) {
         if (SmoothExperiencebar.autohudDetected) return;
         if (SmoothExperiencebar.shouldDisableVanillaExperienceBar()) {
             ci.cancel();
